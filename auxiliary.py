8aefafd932e65d1b06a8460ef9d670d2a5b16e7f
rluver <skyd0608@gmail.com>
2021-07-20 14:37:46 +0900

    file: Upload auxiliary.py

---
+++ auxiliary.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 20 14:26:09 2021
+
+@author: MJH
+"""
+
+
+def categorizer(label):
+    
+    if label == 'positive':
+        return 2
+    elif label == 'neutral':
+        return 1
+    elif label == 'negative':
+        return 0
